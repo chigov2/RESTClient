6f2d10a7710d71685b9aac1b56f1e35e917e85c5
Max2 <chigov@gmail.com>
2022-07-12 18:41:54 +0300

    Initial commit

---
+++ src/main/java/org/chigov/configuration/MyConfig.java
@@ -0,0 +1,9 @@
+package org.chigov.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.chigov")
+public class MyConfig {
+}
---
+++ src/main/java/org/chigov/entity/Employee.java
@@ -0,0 +1,60 @@
+package org.chigov.entity;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private String surname;
+    private int salary;
+    private String department;
+
+    public Employee() {
+    }
+
+    public Employee(String name, String surname, String department, int salary) {
+        this.name = name;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
