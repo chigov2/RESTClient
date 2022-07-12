1405ae210eab85c2b4f13b05341f1067a2e7e62c
chigov2 <chigov@gmail.com>
2022-07-12 21:03:58 +0300

    88

---
+++ src/main/java/org/chigov/Communication.java
@@ -0,0 +1,21 @@
+package org.chigov;
+
+import org.chigov.entity.Employee;
+
+import java.util.List;
+
+public class Communication {
+
+    public List<Employee> showAllEmployees (){
+
+        return  null;
+
+    }
+
+    public Employee getEmployee(int id){
+
+        return null;
+    }
+
+
+}
